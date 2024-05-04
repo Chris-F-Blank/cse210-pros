be27bfdf23ab6a2d926833a6707925f5e6077007
Chris-F-Blank <ful22022@byui.edu>
2024-05-03 22:07:07 -0600

    Modified Program.cs on Prep 5 and polished it a bit, now it displays the user name and favority number squared

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,51 @@
 using System;
 
+class NumberSquaredProgram
+{
+    private string UserName { get; }
+    private int UserNumber { get; }
+    private int SquaredNumber { get; }
+
+    public NumberSquaredProgram(string userName, int userNumber)
+    {
+        UserName = userName;
+        UserNumber = userNumber;
+        SquaredNumber = userNumber * userNumber;
+    }
+
+    public void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    public void DisplayResult()
+    {
+        Console.WriteLine($"{UserName}, the square of your number is {SquaredNumber}");
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the program!");
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        NumberSquaredProgram program = new NumberSquaredProgram(userName, userNumber);
+        program.DisplayResult();
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
     }
 }
